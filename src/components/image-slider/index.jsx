0c62608adaf8e5a512000400bc12e82130a4b3ee
anyars-encarta <anyarsencarta@gmail.com>
2024-02-11 17:05:47 +0000

    Install and correct stylelint and eslint errors

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,88 +1,100 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs';
-import './styles.css'
+import './styles.css';
 
 const ImageSlider = ({ url, limit = 5, page = 1 }) => {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    const [images, setImages] = useState([]);
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const [errorMessage, setErrorMessage] = useState(null);
-    const [loading, setLoading] = useState(false);
+  const fetchImages = async (getUrl) => {
+    try {
+      setLoading(true);
 
-    const fetchImages = async (getUrl) => {
-        try {
-            setLoading(true);
+      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+      const data = await response.json();
 
-            const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
-            const data = await response.json();
-
-            if (data) {
-                setImages(data);
-                setLoading(false);
-            }
-
-        } catch (e) {
-            setErrorMessage(e.message);
-            setLoading(false);
-        }
+      if (data) {
+        setImages(data);
+        setLoading(false);
+      }
+    } catch (e) {
+      setErrorMessage(e.message);
+      setLoading(false);
     }
+  };
 
-    const handlePrevious = () => {
-        setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1)
-    }
+  const handlePrevious = () => {
+    setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1);
+  };
 
-    const handleNext = () => {
-        setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1)
-    }
+  const handleNext = () => {
+    setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1);
+  };
 
-    useEffect(() => {
-        if (url !== '') fetchImages(url)
-    }, [url]);
+  useEffect(() => {
+    if (url !== '') fetchImages(url);
+    // eslint-disable-next-line
+  }, [url]);
 
-    console.log(images);
+  if (loading) {
+    return <div>Loading data! Please wait!!</div>;
+  }
 
-    if (loading) {
-        return <div>Loading data! Please wait!!</div>
-    }
-
-    if (errorMessage !== null) {
-        return <div>Error occured! {errorMessage}</div>
-    }
-
+  if (errorMessage !== null) {
     return (
-        <>
-            <h1 className='slider-title'>Encarta Image Slider</h1>
-
-            <div className='container'>
-                <BsArrowLeftCircleFill onClick={handlePrevious} className='arrow arrow-left' />
-                {
-                    images && images.length ?
-                        images.map((imageItem, index) => (
-                            <img
-                                key={imageItem.id}
-                                alt={imageItem.download_url}
-                                src={imageItem.download_url}
-                                className={currentSlide === index ? 'current-image' : 'current-image hide-current-image'}
-                            />
-                        )) : null
-                }
-                <BsArrowRightCircleFill onClick={handleNext} className='arrow arrow-right' />
-                <span className='circle-indicators'>
-                    {
-                        images && images.length
-                            ? images.map((_, index) => (
-                                <button
-                                    key={index}
-                                    className={currentSlide === index ? 'current-indicator' : 'current-indicator inactive-indicator'}
-                                    onClick={() => setCurrentSlide(index)}
-                                ></button>
-                            ))
-                            : null
-                    }
-                </span>
-            </div>
-        </>
+      <div>
+        Error occured!
+        {errorMessage}
+      </div>
     );
+  }
+
+  return (
+    <>
+      <h1 className="slider-title">Encarta Image Slider</h1>
+
+      <div className="container">
+        <BsArrowLeftCircleFill onClick={handlePrevious} className="arrow arrow-left" />
+        {
+                    images && images.length
+                      ? images.map((imageItem, index) => (
+                        <img
+                          key={imageItem.id}
+                          alt={imageItem.download_url}
+                          src={imageItem.download_url}
+                          className={currentSlide === index ? 'current-image' : 'current-image hide-current-image'}
+                        />
+                      )) : null
+                }
+        <BsArrowRightCircleFill onClick={handleNext} className="arrow arrow-right" />
+        <span className="circle-indicators">
+          {
+                        images && images.length
+                          ? images.map((_, index) => (
+                            // eslint-disable-next-line
+                            <button
+                            // eslint-disable-next-line
+                              key={index}
+                              type="button"
+                              className={currentSlide === index ? 'current-indicator' : 'current-indicator inactive-indicator'}
+                              onClick={() => setCurrentSlide(index)}
+                            />
+                          ))
+                          : null
+                    }
+        </span>
+      </div>
+    </>
+  );
+};
+
+ImageSlider.propTypes = {
+  url: PropTypes.string.isRequired,
+  limit: PropTypes.number.isRequired,
+  page: PropTypes.number.isRequired,
 };
 
 export default ImageSlider;
