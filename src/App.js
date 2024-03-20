60055d59e2a3b6ab57601e80e65efe61a69557a6
anyars-encarta <anyarsencarta@gmail.com>
2024-03-20 15:15:00 +0000

    Correct App.js function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import ImageSlider from './components/image-slider';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <ImageSlider url="https://picsum.photos/v2/list" page="1" limit="10" />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <ImageSlider url="https://picsum.photos/v2/list" page="1" limit="10" />
+  </div>
+);
 
 export default App;
